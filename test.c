5fee063ad2432f86ac40ac5b8b1cfea13ff58d08
Mohamed Gougriz <mohmadgougriz@gmail.com>
2026-05-29 16:30:03 +0100

    start with Numbers

--- test.c
+++ test.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(){
     char name[10];
@@ -10,5 +11,9 @@ int main(){
     scanf("%d",&age);
     printf("Enter your height in meters:");
     scanf("%f",&height);
-    printf("\nYour name is %s\nYour age is %d age\nYour height is %.2f", name, age, height);
+    printf("\nYour name is %s\nYour age is %d age\nYour height is %.2f\n", name, age, height);
+    printf("%f\n", pow(2, 2));
+    printf("%d\n", 5*2);
+    printf("%f\n", sqrt(36));
+    return 0;
 }
